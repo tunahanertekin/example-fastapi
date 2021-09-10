001afca21df237b5a5d564fe0545412fb118e44a
tunahanertekin <tunahan.ertekin13@gmail.com>
2021-09-10 12:50:24 +0300

    any

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ async def read_items():
     return """
     <html>
     
+    
     <body style="background-color: black;">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
